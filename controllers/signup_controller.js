2b8e9283d2d855c7fed101ef2de15e140e98fa4b
Alawode Emmanuel <alawodeemmanuel2@gmail.com>
2021-12-17 12:47:11 +0100

    new one

--- controllers/signup_controller.js
+++ controllers/signup_controller.js
@@ -11,6 +11,14 @@ const userSignup = async (req, res) => {
   try {
     const newUser = await UserAccount.create({
       ...reqData,
+      investments: [
+        {
+          investment_type: 'Stocks',
+          ticker_name: 'TSLA',
+          quantity: 250,
+          invested_amount: 1000,
+        },
+      ],
     });
 
     //   prepare token details
--- models/UserModel.js
+++ models/UserModel.js
@@ -25,22 +25,10 @@ const UserSchema = new Schema(
     },
     investmensts: [
       {
-        investment_type: {
-          type: String,
-          default: 'Stocks',
-        },
-        ticker_name: {
-          type: String,
-          default: 'TLSA',
-        },
-        quantity: {
-          type: Number,
-          default: 3000,
-        },
-        invested_amount: {
-          type: Number,
-          default: 1000,
-        },
+        investment_type: String,
+        ticker_name: String,
+        quantity: Number,
+        invested_amount: Number,
       },
     ],
   },
