c2458a477dfad5d1d9b55937714882b8da9e4cdd
Alawode Emmanuel <alawodeemmanuel2@gmail.com>
2021-12-18 13:53:09 +0100

    ownfowd

--- controllers/get_investment.js
+++ controllers/get_investment.js
@@ -3,16 +3,17 @@ const UserAccount = require('../models/UserModel');
 //--------------------------------------->  Get investments
 const getInvestments = async (req, res) => {
   const UserId = req.body.id;
+  console.log(UserId);
 
   //-->  try to fetch data
   try {
-    const User = await UserAccount.findOne({ _id: UserId })
+    await UserAccount.findOne({ _id: UserId })
       .lean()
       .then((res) => {
         res.json({
           status: 'ok',
           message: 'Data fetched',
-          data: User.investments,
+          data: res.investments,
         });
       })
       .catch((err) => {
