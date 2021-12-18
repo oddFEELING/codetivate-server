7987a143794f96ada093927b892852748694dda5
Alawode Emmanuel <alawodeemmanuel2@gmail.com>
2021-12-18 11:10:39 +0100

    ddd

--- controllers/investment_controller.js
+++ controllers/investment_controller.js
@@ -7,34 +7,36 @@ const addInvestments = async (req, res) => {
   console.log(User);
   const UserId = ReqData.id;
 
-  //-->  GUodate entry
-  try {
-    await UserAccount.updateOne(
-      { _id: UserId },
-      { $set: { investments: [...investments, ReqData.data] } },
-      { returnOriginal: false }
-    )
-      .then((res) => {
-        console.log(`Entry updated`);
-      })
-      .catch((err) => {
-        console.log(`Error updating entry`);
-      });
-  } catch (err) {
-    throw err;
-  }
+  res.send('New test page');
 
-  try {
-    const User = await UserAccount.findOne({ _id: User })
-      .then((res) => {
-        res.json({ status: 'ok', message: 'Data found', data: res });
-      })
-      .then((err) => {
-        res.json({ status: 'bad', message: `Error --> ${err}` });
-      });
-  } catch (err) {
-    throw err;
-  }
+  //-->  GUodate entry
+  // try {
+  //   await UserAccount.updateOne(
+  //     { _id: UserId },
+  //     { $set: { investments: [...investments, ReqData.data] } },
+  //     { returnOriginal: false }
+  //   )
+  //     .then((res) => {
+  //       console.log(`Entry updated`);
+  //     })
+  //     .catch((err) => {
+  //       console.log(`Error updating entry`);
+  //     });
+  // } catch (err) {
+  //   throw err;
+  // }
+
+  // try {
+  //   const User = await UserAccount.findOne({ _id: User })
+  //     .then((res) => {
+  //       res.json({ status: 'ok', message: 'Data found', data: res });
+  //     })
+  //     .then((err) => {
+  //       res.json({ status: 'bad', message: `Error --> ${err}` });
+  //     });
+  // } catch (err) {
+  //   throw err;
+  // }
 };
 
 //--------------------------------------->  Get investments
