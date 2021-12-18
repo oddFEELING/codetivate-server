ac14ef622851d499c72e75d2989622fb31620fac
Alawode Emmanuel <alawodeemmanuel2@gmail.com>
2021-12-18 11:19:25 +0100

    ssss

--- controllers/switch.js
+++ controllers/switch.js
@@ -2,7 +2,7 @@ const signup = require('./signup_controller');
 const login = require('./login_controller');
 const getUser = require('./get_controler');
 const addInvestments = require('./investment_controller');
-const getInvestments = requir('./get_investment.js');
+const getInvestments = require('./get_investment.js');
 
 module.exports = {
   login,
