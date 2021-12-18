75725e5bc11183f5674c30ec2276bd721b5b1eb4
Alawode Emmanuel <alawodeemmanuel2@gmail.com>
2021-12-18 11:21:55 +0100

    fellow

--- controllers/investment_controller.js
+++ controllers/investment_controller.js
@@ -2,41 +2,39 @@ const UserAccount = require('../models/UserModel');
 
 //--------------------------------------->  add investments
 const addInvestments = async (req, res) => {
-  // const ReqData = req.body;
-  // console.log(`The user data is`);
-  // console.log(User);
-  // const UserId = ReqData.id;
+  const ReqData = req.body;
+  console.log(`The user data is`);
+  console.log(User);
+  const UserId = ReqData.id;
 
-  res.send('New test page');
+  // -->  try Update entry
+  try {
+    await UserAccount.updateOne(
+      { _id: UserId },
+      { $set: { investments: [...investments, ReqData.data] } },
+      { returnOriginal: false }
+    )
+      .then((res) => {
+        console.log(`Entry updated`);
+      })
+      .catch((err) => {
+        console.log(`Error updating entry`);
+      });
+  } catch (err) {
+    throw err;
+  }
 
-  //-->  GUodate entry
-  // try {
-  //   await UserAccount.updateOne(
-  //     { _id: UserId },
-  //     { $set: { investments: [...investments, ReqData.data] } },
-  //     { returnOriginal: false }
-  //   )
-  //     .then((res) => {
-  //       console.log(`Entry updated`);
-  //     })
-  //     .catch((err) => {
-  //       console.log(`Error updating entry`);
-  //     });
-  // } catch (err) {
-  //   throw err;
-  // }
-
-  // try {
-  //   const User = await UserAccount.findOne({ _id: User })
-  //     .then((res) => {
-  //       res.json({ status: 'ok', message: 'Data found', data: res });
-  //     })
-  //     .then((err) => {
-  //       res.json({ status: 'bad', message: `Error --> ${err}` });
-  //     });
-  // } catch (err) {
-  //   throw err;
-  // }
+  try {
+    const User = await UserAccount.findOne({ _id: User })
+      .then((res) => {
+        res.json({ status: 'ok', message: 'Data found', data: res });
+      })
+      .then((err) => {
+        res.json({ status: 'bad', message: `Error --> ${err}` });
+      });
+  } catch (err) {
+    throw err;
+  }
 };
 
 module.exports = addInvestments;
